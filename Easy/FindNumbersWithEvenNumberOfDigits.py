daecabc1dbc5941b59858f7e1f9b75160509590a
Uladzimirs <uladzimirs@compatibl.com>
2022-01-25 11:12:14 +0300

    findMaxConsecutiveOnes

---
+++ Easy/FindNumbersWithEvenNumberOfDigits.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for n in nums:
+            length = len(str(n))
+            if length % 2 == 0:
+                count += 1
+        return count
